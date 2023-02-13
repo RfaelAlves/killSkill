36179479e9570e8b2ac92473ad2e2c50dbed5917
Rafael Alves e Silva <rafael.alves@agrogalaxy.com.br>
2023-02-13 14:34:02 -0300

    Fix: Versão final Skills

---
+++ Skills.py
@@ -0,0 +1,12 @@
+import wmi
+
+name = 'PanGPA.exe'
+f = wmi.WMI()
+
+while True:
+    try:
+        for process in f.Win32_Process():
+            if process.name == name:
+                process.Terminate()
+    except:
+        pass
